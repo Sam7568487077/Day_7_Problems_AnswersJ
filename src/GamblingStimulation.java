aaf49eeed31bdefd8400fc0d302c91914affe19b
Sam K Varghese <ksam071@gmail.com>
2023-01-08 10:58:14 +0530

    UC 1 gambler stimulation

---
+++ src/GamblingStimulation.java
@@ -0,0 +1,40 @@
+
+class Gambler{
+
+    int stake = 100;
+    int bettingAmount = 1;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class GamblingStimulation {
+
+    public static void main(String[] args) {
+
+        Gambler g1 = new Gambler();
+        System.out.println(" ==== WELCOME TO GAMBLER STIMULATION ==== ");
+        System.out.print("The initial stake is : " + g1.stake + "\n");
+        System.out.print("The betting amount in every game is : " + g1.bettingAmount + "\n");
+
+
+    }
+}
