c6008f2fae8f565fd541f4911fcf155356f74a7a
Sam K Varghese <ksam071@gmail.com>
2023-01-08 11:35:03 +0530

    Uc 2 gambler outcome

--- src/GamblingStimulation.java
+++ src/GamblingStimulation.java
@@ -1,39 +1,48 @@
 
-class Gambler{
+class Gambler {
 
     int stake = 100;
     int bettingAmount = 1;
 
 
 
+    Gambler() {
+        System.out.print("The initial stake is : " + stake + "\n");
+        System.out.print("The betting amount in every game is : " + bettingAmount + "\n");
+    }
+
+
+    public void outcomeGenerator () {
+
+        System.out.println(" ================== YOU ARE PLACING THE BET ==================" + " \n");
+
+        int outcome = (int) Math.floor(Math.random() * 10) % 2;
+        switch (outcome){
+
+            case 0:
+                System.out.println("You have won the bet");
+                System.out.println("\n");
+                stake=stake+bettingAmount;
+                break;
+            case 1:
+                System.out.println("You have lost the bet ");
+                System.out.println("\n");
+                stake=stake-bettingAmount;
+                break;
+        }
+        System.out.println("Your current stake is: " + stake);
+
+
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 public class GamblingStimulation {
 
     public static void main(String[] args) {
 
         Gambler g1 = new Gambler();
-        System.out.println(" ==== WELCOME TO GAMBLER STIMULATION ==== ");
-        System.out.print("The initial stake is : " + g1.stake + "\n");
-        System.out.print("The betting amount in every game is : " + g1.bettingAmount + "\n");
+        g1.outcomeGenerator();
+
 
 
     }
