c7d1a7a06fae5ce2408871721d9aad73b89c7c04
Sam K Varghese <ksam071@gmail.com>
2023-01-08 23:57:30 +0530

    ShowBoard to user

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,23 +6,36 @@ class TicTacToe01{
     char [] boardSpaces = new char[10];
 
 
-    TicTacToe01(){
+    TicTacToe01() {
 
-        for (int i=1;i<boardSpaces.length;i++)
-        {
+        for (int i = 1; i < boardSpaces.length; i++) {
 
             boardSpaces[i] = 'N';
         }
-        for(int i=0;i < (boardSpaces.length-1)/3;i++)
-        {
-            for(int j=3*i+1;j<3*i+1+3;j++) {
-                System.out.print( boardSpaces[j] + "" + j + "   ");
+        for (int i = 0; i < (boardSpaces.length - 1) / 3; i++) {
+            for (int j = 3 * i + 1; j < 3 * i + 1 + 3; j++) {
+                System.out.print(boardSpaces[j] + "" + j + "   ");
 
             }
             System.out.println("\n");
         }
 
     }
+
+        void show_Board(){
+
+            for(int i=0;i < (boardSpaces.length-1)/3;i++)
+            {
+                for(int j=3*i+1;j<3*i+1+3;j++) {
+                    System.out.print( boardSpaces[j] + "" + j + "   ");
+
+                }
+                System.out.println("\n");
+            }
+
+        }
+
+
 
 
     void userChoice(){
@@ -62,6 +75,7 @@ public class TicTacToe {
     public static void main(String[] args) {
         TicTacToe01 t1 = new TicTacToe01();
         t1.userChoice();
+        t1.show_Board();
 
 
     }
